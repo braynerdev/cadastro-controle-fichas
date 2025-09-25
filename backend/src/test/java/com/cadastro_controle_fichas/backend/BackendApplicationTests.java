42c8324a1c75fced7afaad140601375cf903afcc
braynerdev <dev.brayner@outlook.com>
2025-09-24 22:06:41 -0300

    estrutura inicial do projeto

---
+++ backend/src/test/java/com/cadastro_controle_fichas/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cadastro_controle_fichas.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
